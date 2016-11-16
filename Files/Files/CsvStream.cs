1af89e23b09f7c94f2ff73e35e760aeb6737c755
Kira Tanaka <suein_i@mail.ru>
2016-11-16 22:28:28 +0500

    Create. Task 1.

---
+++ Files/Files/CsvStream.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    internal class CsvStream
+    {
+        internal IEnumerable<string[]> ReadCsv1()
+        {
+            using (var stream = new StreamReader("airquality.csv"))
+            {
+                while (true)
+                {
+                    var lineValues = stream.ReadLine();
+                    if (lineValues == null)
+                    {
+                        stream.Close();
+                        yield break;
+                    }
+                    yield return lineValues.Split(',').ToList().Select(x =>
+                    {
+                        x = (x == "NA") ? null : x;
+                        return x;
+                    }).ToArray();
+                }
+            }      
+        }
+
+    }
+}
---
+++ Files/Files/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CsvStream stream = new CsvStream();
+            List<string[]> list=stream.ReadCsv1().ToList();
+        }
+    }
+}
